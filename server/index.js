3eb21b053d29ebb135f3f0ba01247a0a4e9b4eca
julfinch <julfinch@gmail.com>
2023-02-08 01:38:16 +0800

    app.use for helmet CSP

--- server/index.js
+++ server/index.js
@@ -36,6 +36,60 @@ app.use(expressCspHeader({
         'img-src': [expressCspHeader.SELF], 
     } 
 }));  
+app.use(
+    helmet.contentSecurityPolicy({
+      useDefaults: false,
+      "block-all-mixed-content": true,
+      "upgrade-insecure-requests": true,
+      directives: {
+        "default-src": [
+            "'self'"
+        ],
+        "base-uri": "'self'",
+        "font-src": [
+            "'self'",
+            "https:",
+            "data:"
+        ],
+        "frame-ancestors": [
+            "'self'"
+        ],
+        "img-src": [
+            "'self'",
+            "data:"
+        ],
+        "object-src": [
+            "'none'"
+        ],
+        "script-src": [
+            "'self'",
+            "https://cdnjs.cloudflare.com"
+        ],
+        "script-src-attr": "'none'",
+        "style-src": [
+            "'self'",
+            "https://cdnjs.cloudflare.com"
+        ],
+      },
+    }),
+    helmet.dnsPrefetchControl({
+        allow: true
+    }),
+    helmet.frameguard({
+        action: "deny"
+    }),
+    helmet.hidePoweredBy(),
+    helmet.hsts({
+        maxAge: 123456,
+        includeSubDomains: false
+    }),
+    helmet.ieNoOpen(),
+    helmet.noSniff(),
+    helmet.referrerPolicy({
+        policy: [ "origin", "unsafe-url" ]
+    }),
+    helmet.xssFilter()
+)
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
