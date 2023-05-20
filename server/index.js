40878c2bac0b618d6c71aa68d8967d7342e88144
julfinch <julfinch@gmail.com>
2023-05-20 23:05:57 +0800

    update docker

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
-import multer from "multer"
 import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
@@ -91,16 +90,6 @@ app.use(
     helmet.xssFilter()
 )
 
-/* FILE STORAGE */
-const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, "public/assets");
-	},
-	filename: function (req, file, cb) {
-		cb(null, file.originalname);
-	},
-});
-const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", register);
