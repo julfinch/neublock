8d56943b64dcae6d8fa740f310558ba139d188ae
julfinch <julfinch@gmail.com>
2023-05-22 01:59:45 +0800

    Revert "update docker"
    This reverts commit 40878c2bac0b618d6c71aa68d8967d7342e88144.

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
+import multer from "multer"
 import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
@@ -90,6 +91,16 @@ app.use(
     helmet.xssFilter()
 )
 
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "public/assets");
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	},
+});
+const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", register);
